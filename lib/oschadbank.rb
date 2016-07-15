bdc6cc1e96f02b86cf14ed6aa8c97a6565d021e0
Roman Khrebtov <roman@alltmb.ru>
2016-07-15 13:20:27 +0300

    Init gem

---
+++ lib/oschadbank.rb
@@ -0,0 +1,5 @@
+require "oschadbank/version"
+
+module Oschadbank
+  # Your code goes here...
+end
---
+++ test/oschadbank_test.rb
@@ -0,0 +1,11 @@
+require 'test_helper'
+
+class OschadbankTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Oschadbank::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
