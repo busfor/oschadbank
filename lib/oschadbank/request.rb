907dd4600afd201d0740dc00161be600511392f2
Roman Khrebtov <roman@alltmb.ru>
2016-07-27 12:46:42 +0300

    Add certificates to request

--- lib/oschadbank/request.rb
+++ lib/oschadbank/request.rb
@@ -18,9 +18,16 @@ module Oschadbank
     private
 
     def make_request
-      Faraday.post(request_url, request_params)
+      connection = Faraday::Connection.new(request_url, ssl: {
+        ca_file: cert_file,
+      })
+      connection.post(request_url, request_params)
     rescue Faraday::Error => e
       raise RequestError, e.message
+    end
+
+    def cert_file
+      File.expand_path("../../data/cert.pem", File.dirname(__FILE__))
     end
   end
 end
